a6f2cd8dfbc1ad2ca8f696a92b8b3f7d9efb5c10
Somil-Jain24 <somiljain024@gmail.com>
2025-11-23 13:02:24 +0530

    GCD Of Number

---
+++ backtracking/GCD.java
@@ -0,0 +1,15 @@
+public class GCD {
+    public static int gcd(int a , int b){
+        if(a<0||b<0){
+            return -1;
+        }
+        if(b==0){
+            return a;
+        }else 
+            return gcd(b, a%b);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(gcd(48, 18));
+    }
+}
